4c43055f1d7aabafb8e2ce10c699266e11dce8f5
Jason Katz <jkatz94@gmail.com>
2015-10-10 20:40:41 -0400

    Getting colors to work

--- Game.cpp
+++ Game.cpp
@@ -20,10 +20,15 @@ Game::Game(Player * p1, Player * p2, int limit) {
 }
 
 void Game::PrintBoard() {
-	char normal [] = { 0x1b, '[', '0', ';', '3', '9', 'm', 0 };
+	char normal[] = { 0x1b, '[', '0', ';', '3', '9', 'm', 0 };
 	char blue[] = { 0x1b, '[', '1', ';', '3', '4', 'm', 0 };
 	char pink[] = { 0x1b, '[', '1', ';', '3', '4', 'm', 0 };
+    vector<char *> colors;
+    colors.push_back(&normal[0]);
+    colors.push_back(&blue[0]);
+    colors.push_back(&pink[0]);
 	char symbols[3] = { 'a', '*', '-' };
+    vector<vector<int>> board = currentState.GetBoard();
 	cout << "---------------------------------" << endl;
 	for (int i = 0; i < 8; ++i) {
 		for (int j = 0; j < 8; ++j) {
@@ -31,7 +36,7 @@ void Game::PrintBoard() {
 		}
 		cout << "|" << endl;
 		for (int j = 0; j < 8; ++j) {
-			cout << "| " << symbols[currentState.GetBoard()[i][j] % 3] << " ";
+			cout << "| " << colors[board[i][j] % 3] << symbols[board[i][j] % 3] << normal << " ";
 		}
 		cout << "|" << endl;
 		for (int j = 0; j < 8; ++j) {
